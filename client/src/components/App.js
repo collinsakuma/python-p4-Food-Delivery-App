fc809869abc63432feaefc347bc402badf9f6e30
Collin Sakuma <collinmsakuma@gmail.com>
2023-04-18 11:29:17 -0700

    initial commit

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,11 @@ import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 
 function App() {
-  // Code goes here!
+  return (
+    <div>
+      <h1>This is the start of our App</h1>
+    </div>
+  );
 }
 
 export default App;
